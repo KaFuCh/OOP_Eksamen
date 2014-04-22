ef22ddda35a12d3377cab0a8e88d9722be4a7357
Hutli <jensstaermose@hotmail.com>
2014-04-22 09:37:50 +0200

    CVR and CPR changged

--- OOP_Eksamen/OOP_Eksamen/Program.cs
+++ OOP_Eksamen/OOP_Eksamen/Program.cs
@@ -14,8 +14,8 @@ namespace OOP_Eksamen {
 
         public static void testExample() {
             AuctionHouse testAuctionHouse = new AuctionHouse();
-            List<Seller> sellers = new List<Seller> {new BusinessSeller("12629885", 8300, 1000000, 500000), new PrivateSeller("0104931839", 9000, 100000)};
-            List<Buyer> buyers = new List<Buyer>{new BusinessBuyer("48570658", 300000, 300000), new PrivateBuyer("0606941137", 40000)};
+            List<Seller> sellers = new List<Seller> { new BusinessSeller("55133018", 8300, 1000000, 500000), new PrivateSeller("0708912247", 9000, 100000) };
+            List<Buyer> buyers = new List<Buyer> { new BusinessBuyer("32264328", 300000, 300000), new PrivateBuyer("1102931129", 40000) };
 
             int testBusKey = testAuctionHouse.SetSale(new Bus("Mercedes Benz Travego", "AB87232", 2005, Vehicle.LicenceType.D, Vehicle.FuelType.Diesel, 120000, 8000), sellers[0]);
             int testCarPrivateKey = testAuctionHouse.SetSale(new CarPrivate("Audi A2", "SD82738", 1996, Vehicle.LicenceType.B, Vehicle.FuelType.Petrol, 32000), sellers[1]);
@@ -60,68 +60,68 @@ namespace OOP_Eksamen {
                     Console.WriteLine("\nAuctions House's balance is {0:C}", testAuctionHouse.Balance);
                     break;
                 case '2':
-                Console.WriteLine("\nSearch examples:\n" +
-                                  "1: String search example\n" +
-                                  "2: Min seats and toilet search\n" +
-                                  "3: Big licence type and maximal weight\n" +
-                                  "4: Private cars; max km and max price and sorted\n" +
-                                  "5: Zip code radius\n" +
-                                  "6: Energy class average\n");
-                do
-                    key = Console.ReadKey().KeyChar;
-                while(key != '1' && key != '2' && key != '3' && key != '4' && key != '5' && key != '6');
-                List<Vehicle> sortedList = new List<Vehicle>();
-                switch(key) {
-                    case '1':
-                        Console.WriteLine("\nEnter string:");
-                        string searchString = Console.ReadLine();
-                        sortedList = testAuctionHouse.SearchString(searchString);
-                        break;
-                    case '2':
-                        Console.WriteLine("\nEnter minimum seats:");
-                        uint minSeats = Convert.ToUInt32(Console.ReadLine());
-                        Console.WriteLine("Toilet? (Y/N)");
-                        char hasToiletChar = Console.ReadKey().KeyChar;
-                        bool hasToilet = false;
+                    Console.WriteLine("\nSearch examples:\n" +
+                                      "1: String search example\n" +
+                                      "2: Min seats and toilet search\n" +
+                                      "3: Big licence type and maximal weight\n" +
+                                      "4: Private cars; max km and max price and sorted\n" +
+                                      "5: Zip code radius\n" +
+                                      "6: Energy class average\n");
+                    do
+                        key = Console.ReadKey().KeyChar;
+                    while(key != '1' && key != '2' && key != '3' && key != '4' && key != '5' && key != '6');
+                    List<Vehicle> sortedList = new List<Vehicle>();
+                    switch(key) {
+                        case '1':
+                            Console.WriteLine("\nEnter string:");
+                            string searchString = Console.ReadLine();
+                            sortedList = testAuctionHouse.SearchString(searchString);
+                            break;
+                        case '2':
+                            Console.WriteLine("\nEnter minimum seats:");
+                            uint minSeats = Convert.ToUInt32(Console.ReadLine());
+                            Console.WriteLine("Toilet? (Y/N)");
+                            char hasToiletChar = Console.ReadKey().KeyChar;
+                            bool hasToilet = false;
 
-                        if(hasToiletChar == 'Y' || hasToiletChar == 'y')
-                            hasToilet = true;
-                        else
-                            hasToilet = false;
+                            if(hasToiletChar == 'Y' || hasToiletChar == 'y')
+                                hasToilet = true;
+                            else
+                                hasToilet = false;
 
-                        sortedList = testAuctionHouse.SearchSeatsAndToilets(minSeats, hasToilet);
-                        break;
-                    case '3':
-                        Console.WriteLine("\nEnter max weight:");
-                        int maxWeight = Convert.ToInt32(Console.ReadLine());
-                        sortedList = testAuctionHouse.SearchLicence(maxWeight);
-                        break;
-                    case '4':
-                        Console.WriteLine("\nEnter max km");
-                        int maxKm = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter max price");
-                        decimal maxPrice = Convert.ToDecimal(Console.ReadLine());
-                        sortedList = testAuctionHouse.SearchPrivateCar(maxKm, maxPrice);
-                        break;
-                    case '5':
-                        Console.WriteLine("\nEnter zipcode");
-                        int zipCode = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("\nEnter range");
-                        int range = Convert.ToInt32(Console.ReadLine());
-                        sortedList = testAuctionHouse.SearchZipCode(range, zipCode);
-                        break;
-                    case '6':
-                        Console.WriteLine("\nEnergy classes:");
-                        foreach(Vehicle v in testAuctionHouse.ForSale.Values) {
-                            Console.WriteLine(v.EnergyClass);
-                        }
-                        Console.WriteLine("\nAverage energy Class: " + testAuctionHouse.averageEnergyClassType());
-                        break;
-                }
-                foreach(Vehicle v in sortedList) {
-                    Console.WriteLine(v);
-                }
-                break;
+                            sortedList = testAuctionHouse.SearchSeatsAndToilets(minSeats, hasToilet);
+                            break;
+                        case '3':
+                            Console.WriteLine("\nEnter max weight:");
+                            int maxWeight = Convert.ToInt32(Console.ReadLine());
+                            sortedList = testAuctionHouse.SearchLicence(maxWeight);
+                            break;
+                        case '4':
+                            Console.WriteLine("\nEnter max km");
+                            int maxKm = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter max price");
+                            decimal maxPrice = Convert.ToDecimal(Console.ReadLine());
+                            sortedList = testAuctionHouse.SearchPrivateCar(maxKm, maxPrice);
+                            break;
+                        case '5':
+                            Console.WriteLine("\nEnter zipcode");
+                            int zipCode = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("\nEnter range");
+                            int range = Convert.ToInt32(Console.ReadLine());
+                            sortedList = testAuctionHouse.SearchZipCode(range, zipCode);
+                            break;
+                        case '6':
+                            Console.WriteLine("\nEnergy classes:");
+                            foreach(Vehicle v in testAuctionHouse.ForSale.Values) {
+                                Console.WriteLine(v.EnergyClass);
+                            }
+                            Console.WriteLine("\nAverage energy Class: " + testAuctionHouse.averageEnergyClassType());
+                            break;
+                    }
+                    foreach(Vehicle v in sortedList) {
+                        Console.WriteLine(v);
+                    }
+                    break;
             }
 
             Console.ReadLine();
